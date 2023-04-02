a8f8302106811928ffde98c5461a02e0b132b799
Amigo (WhiteLuckBringers) <white.luckers@gmail.com>
2023-04-02 17:50:54 +0900

    feature: add slider input component

---
+++ src/components/inputs/Slider.tsx
@@ -0,0 +1,33 @@
+import { Component } from 'solid-js';
+
+interface Props {
+  id: string;
+  label: string;
+  min: number;
+  max: number;
+  step: number;
+  value: number;
+  onChange: (next: number) => void;
+}
+export const SliderInput: Component<Props> = (props) => {
+  return (
+    <div>
+      <label for={props.id}>{props.label}</label>
+      <div class="w-full flex flex-row gap-x-2 items-center mt-2">
+        <span class="w-10">{props.value}</span>
+        <input
+          class="flex-1 min-w-0"
+          type="range"
+          id={props.id}
+          min={props.min}
+          max={props.max}
+          step={props.step}
+          value={props.value}
+          onInput={(ev) => {
+            props.onChange(Number(ev.currentTarget.value));
+          }}
+        />
+      </div>
+    </div>
+  );
+};
--- src/views/BasicConfig.tsx
+++ src/views/BasicConfig.tsx
@@ -4,12 +4,24 @@ import { useAppViewModel } from '../viewModels';
 import { AllExportFileTypeEnum } from '../models';
 import clsx from 'clsx';
 import { windi } from '../windi';
+import { SliderInput } from '../components/inputs/Slider';
 
 export const BasicConfig: Component<{ class?: string }> = (props) => {
-  const { parameters, setParameters } = useAppViewModel();
+  const { parameters, setParameters, reset } = useAppViewModel();
 
   return (
     <div class={clsx(windi`w-full p-2 overflow-y-auto overflow-x-hidden flex flex-col gap-y-4`, props.class)}>
+      <SliderInput
+        id="threshold"
+        label="境界検出で使うしきい値"
+        min={1}
+        max={254}
+        step={1}
+        value={parameters.threshold}
+        onChange={(v) => {
+          setParameters('threshold', () => v);
+        }}
+      />
       <NumberStepper
         id="patch-size"
         label="一度に動かす範囲 (px)"
@@ -26,6 +38,7 @@ export const BasicConfig: Component<{ class?: string }> = (props) => {
         options={AllExportFileTypeEnum}
         optionLabelGetter={(opt) => opt}
       />
+      <button onClick={reset}>設定をデフォルト値にリセット</button>
     </div>
   );
 };
